861b9e69d566cd77a3b890a4b37bc7dcb630cd2c
Kamil Janusz <kamiljanusztc@gmail.com>
2022-10-14 18:04:24 +0200

    Init

---
+++ src/main/java/com/app/findcarbackend/FindCarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.findcarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindCarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindCarBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/findcarbackend/FindCarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.findcarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindCarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
